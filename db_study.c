f4731ec411aaddd590236e9798cbd7af1f275d77
wuxi <a1363182250@163.com>
2022-08-25 22:44:34 +0800

    our first tests(and bugs) wonderful

--- db_study.c
+++ db_study.c
@@ -88,7 +88,8 @@ read_input(
   ssize_t bytes_read =
       getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
 
-  if (bytes_read <= 0) {
+  if (bytes_read <= 0) 
+  {
     printf("Error reading input\n");
     exit(EXIT_FAILURE);
   }
@@ -121,6 +122,45 @@ do_meta_command(
 }
 
 prepare_result_e 
+prepare_insert(
+  input_buffer_t* input_buffer, 
+  statement_t*    statement
+) 
+{
+  statement->type = STATEMENT_INSERT;
+
+  char* keyword   = strtok(input_buffer->buffer, " ");
+  char* id_string = strtok(NULL, " ");
+  char* username  = strtok(NULL, " ");
+  char* email     = strtok(NULL, " ");
+
+  if (id_string == NULL || username == NULL || email == NULL) 
+  {
+    return PREPARE_SYNTAX_ERROR;
+  }
+
+  int id = atoi(id_string);
+  if(id < 0)
+  {
+    return PREPARE_NEGATIVE_ID;
+  }
+  if (strlen(username) > COLUMN_USERNAME_SIZE) {
+    return PREPARE_STRING_TOO_LONG;
+  }
+  if (strlen(email) > COLUMN_EMAIL_SIZE) 
+  {
+    return PREPARE_STRING_TOO_LONG;
+  }
+
+  statement->row_to_insert.id = id;
+  strcpy(statement->row_to_insert.username, username);
+  strcpy(statement->row_to_insert.email, email);
+
+  return PREPARE_SUCCESS;
+}
+
+
+prepare_result_e 
 prepare_statement(
   input_buffer_t*   input_buffer,
   statement_t*      statement
@@ -128,14 +168,7 @@ prepare_statement(
 {
   if (strncmp(input_buffer->buffer, "insert", 6) == 0) 
   {
-    statement->type = STATEMENT_INSERT;
-    int args_assigned = sscanf(input_buffer->buffer,"insert %d %s %s",
-        &(statement->row_to_insert.id),statement->row_to_insert.username,statement->row_to_insert.email);
-    if(args_assigned < 3)
-    {
-      return PREPARE_SYNTAX_ERROR;
-    }
-    return PREPARE_SUCCESS;
+    return prepare_insert(input_buffer, statement);
   }
   if (strcmp(input_buffer->buffer, "select") == 0)
    {
@@ -255,6 +288,12 @@ int main(
     {
       case (PREPARE_SUCCESS):
         break;
+      case (PREPARE_NEGATIVE_ID):
+        printf("ID must be positive.\n"); 
+        continue; 
+      case (PREPARE_STRING_TOO_LONG):
+        printf("String is too long.\n");
+        continue;
       case (PREPARE_SYNTAX_ERROR):
         printf("Syntax error. Could not parse statement.\n");
         continue;
