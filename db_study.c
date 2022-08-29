01b0e3013dc4c6803daf5dc818f387291a40c8ed
wuxi <a1363182250@163.com>
2022-08-29 21:17:47 +0800

    binary search and duplicate keys

--- db_study.c
+++ db_study.c
@@ -9,8 +9,9 @@
 
 #define size_of_attribute(Struct, Attribute) sizeof(((Struct*)0)->Attribute)
 
-void*
-get_page(pager_t* pager, uint32_t page_num);
+void*get_page(pager_t* pager, uint32_t page_num);
+
+void set_node_type(void* node, node_type_e type);
 
 const uint32_t ID_SIZE        = size_of_attribute(row_t, id);
 const uint32_t USERNAME_SIZE  = size_of_attribute(row_t, username);
@@ -99,6 +100,7 @@ initialize_leaf_node(
   void*     node
 )
 {
+  set_node_type(node, NODE_LEAF);
   *leaf_node_num_cells(node) = 0;
 }
 
@@ -138,21 +140,102 @@ table_start(
   return cursor;
 }
 
-cursor_t*
-table_end(
-  table_t* table
+// cursor_t*
+// table_end(
+//   table_t* table
+// )
+// {
+//   cursor_t* cursor      = malloc(sizeof(cursor_t));
+//   cursor->table         = table;
+// //  cursor->row_num   = table->num_rows;
+//   cursor->page_num      = table->root_page_num;
+
+//   void*     root_node   = get_page(table->pager, table->root_page_num);
+//   uint32_t  num_cells   = *leaf_node_num_cells(root_node);
+//   cursor->cell_num      = num_cells;
+//   cursor->end_of_table  = true;
+//   return cursor;
+// }
+
+node_type_e
+get_node_type(
+  void* node
 )
 {
-  cursor_t* cursor      = malloc(sizeof(cursor_t));
-  cursor->table         = table;
-//  cursor->row_num   = table->num_rows;
-  cursor->page_num      = table->root_page_num;
+  uint8_t value = *((uint8_t*)(node + NODE_TYPE_OFFSET));
+  return (node_type_e)value;
+}
 
-  void*     root_node   = get_page(table->pager, table->root_page_num);
-  uint32_t  num_cells   = *leaf_node_num_cells(root_node);
-  cursor->cell_num      = num_cells;
-  cursor->end_of_table  = true;
+void 
+set_node_type(
+  void*       node,
+  node_type_e type
+)
+{
+  uint8_t value = type;
+
+  *((uint8_t*)(node + NODE_TYPE_OFFSET)) = value;
+}
+
+cursor_t*
+leaf_node_find(
+  table_t*    table,
+  uint32_t    page_num,
+  uint32_t    key
+)
+{
+  void*     node      = get_page(table->pager, page_num);
+  uint32_t  num_cells = *leaf_node_num_cells(node);
+  cursor_t* cursor    = malloc(sizeof(cursor_t));
+
+  cursor->table       = table;
+  cursor->page_num    = page_num;
+
+  //binary search
+  uint32_t    min_index           = 0;
+  uint32_t    one_past_max_index  = num_cells;
+
+  while (one_past_max_index != min_index)
+  {
+    uint32_t index        = (min_index + one_past_max_index) / 2;
+    uint32_t key_at_index = *leaf_node_key(node, index);
+    if(key == key_at_index)
+    {
+      cursor->cell_num = index;
+      return cursor;
+    }
+    if(key < key_at_index)
+    {
+      one_past_max_index = index;
+    }
+    else
+    {
+      min_index = index + 1;
+    }
+  }
+  
+  cursor->cell_num = min_index;
   return cursor;
+}
+
+cursor_t*
+table_find(
+  table_t*    table,
+  uint32_t    key
+)
+{
+  uint32_t  root_page_num = table->root_page_num;
+  void*     root_node     = get_page(table->pager, root_page_num);
+
+  if(get_node_type(root_node) == NODE_LEAF)
+  {
+    return leaf_node_find(table, root_page_num, key);
+  }
+  else
+  {
+    printf("Need to implement searching an internal node\n");
+    exit(EXIT_FAILURE);
+  }
 }
 
 void 
@@ -521,16 +604,30 @@ execute_insert(
 ) 
 {
   void* node = get_page(table->pager, table->root_page_num);
-  if ((*leaf_node_num_cells(node)) >= LEAF_NODE_MAX_CELLS) 
-  {
+  // if ((*leaf_node_num_cells(node)) >= LEAF_NODE_MAX_CELLS) 
+  // {
+  uint32_t num_cells = (*leaf_node_num_cells(node));
+  if(num_cells >= LEAF_NODE_MAX_CELLS)
+  {  
     return EXECUTE_TABLE_FULL;
   }
 
   row_t*    row_to_insert = &(statement->row_to_insert);
-  cursor_t* cursor        = table_end(table);
+//  cursor_t* cursor        = table_end(table);
 
   // serialize_row(row_to_insert, cursor_value(cursor));
   // table->num_rows += 1;
+
+  uint32_t  key_to_insert = row_to_insert->id;
+  cursor_t* cursor        = table_find(table, key_to_insert);
+  if(cursor->cell_num < num_cells)
+  {
+    uint32_t key_at_index = *leaf_node_key(node, cursor->cell_num);
+    if(key_at_index == key_to_insert)
+    {
+      return EXECUTE_DUPLICATE_KEY;
+    }
+  }
   leaf_node_insert(cursor, row_to_insert->id, row_to_insert);
   free(cursor);
   return EXECUTE_SUCCESS;
@@ -655,13 +752,13 @@ int main(
     char*   argv[]
 ) 
 {
-  // if(argc < 2)
-  // {
-  //   printf("Must supply a database filename.\n");
-  //   exit(EXIT_FAILURE);
-  // }
-  // char*     filename  = argv[1];
-  char*     filename  = "mydb.db";
+  if(argc < 2)
+  {
+    printf("Must supply a database filename.\n");
+    exit(EXIT_FAILURE);
+  }
+  char*     filename  = argv[1];
+  //char*     filename  = "mydb.db";
   table_t*  table     = db_open(filename);
   input_buffer_t* input_buffer  = new_input_buffer();
   while (true) 
@@ -703,6 +800,9 @@ int main(
     {
     case (EXECUTE_SUCCESS):
       printf("Executed.\n");
+      break;
+    case (EXECUTE_DUPLICATE_KEY):
+      printf("Error: Duplicate key.\n");
       break;
     case (EXECUTE_TABLE_FULL):
       printf("Error: Table full.\n");
